5e73887e251a8efaff5800d074e8ade50395185d
CN11 <codenation@CN11s-MacBook-Pro.local>
2020-02-27 12:06:44 +0000

    a sheet that lists the various operators

---
+++ cheatsheet.js
@@ -0,0 +1,17 @@
+// = assigns an oprator
+// == checks if values are equal
+// === checks if values and data types are equal
+// != checks if values are not equal
+// !== checks if values and datat types are not equal
+// + adds values
+// - subtracts values
+// * multiplies values
+// / divides values
+// ++ adds 1 to a value
+// -- subtracts one from a value
+// > more than
+// >= more than or equal to
+// < less than
+// <= less than or equal to
+// && and
+// || or
